cc13540250afc9dfcacc5a149d1ae2516c3bff66
Carlos Andres Perez <carlosaperez1997@gmail.com>
2019-07-20 00:34:01 -0500

    primera creacionde los componentes

---
+++ src/app/pages/home/home.page.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.page.html',
+  styleUrls: ['./home.page.scss'],
+})
+export class HomePage implements OnInit {
+
+  componentes: Array<Componente>;
+  constructor() {
+    this.componentes = [
+      {
+        icon: 'american-football',
+        name: 'Action Sheet',
+        redirectTo: '/action-sheet'
+      },
+      {
+        icon: 'appstore',
+        name: 'Alert',
+        redirectTo: '/alert'
+      }
+    ];
+  }
+
+  ngOnInit() {
+  }
+
+}
+
+interface Componente {
+  icon: string;
+  name: string;
+  redirectTo: string;
+}
